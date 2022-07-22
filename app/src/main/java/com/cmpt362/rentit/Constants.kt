12a6390d43300c67cae70a23ab07544898fff6fb
Kenneth Liang <kenneth-liang@hotmail.com>
2022-07-22 02:31:50 -0700

    can login, signup and log out

---
+++ app/src/main/java/com/cmpt362/rentit/Constants.kt
@@ -0,0 +1,9 @@
+package com.cmpt362.rentit
+
+object Constants {
+    const val INVALID_EMAIL_FORMAT_ERROR = "Invalid email format"
+    const val NO_PASSWORD_ENTERED_ERROR = "Please enter a password"
+    const val MIN_PASSWORD_LENGTH = 6
+    const val PASSWORD_LENGTH_ERROR = "Password must be at least ${MIN_PASSWORD_LENGTH} characters long"
+
+}
