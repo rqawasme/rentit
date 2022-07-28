e87605184b59751d488d9c09d8864e7688111ca6
knakano <kei_nakano@sfu.ca>
2022-07-27 21:01:15 -0700

    Image gallery initial commit

--- app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
+++ app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
@@ -1,4 +1,4 @@
-package com.cmpt362.rentit
+package com.cmpt362.rentit.details
 
 import android.os.Bundle
 import android.util.Log
@@ -6,27 +6,62 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.bumptech.glide.Glide
+import com.cmpt362.rentit.R
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class DetailActivity:AppCompatActivity() {
     private lateinit var db: FirebaseDatabase
     private var name:String?=""
     private var price:Float? =0f
     private var description:String? =""
+    lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        var reference = Firebase.storage.reference
+
+
+        viewPager = findViewById(R.id.detail_viewpager)
+
+
         val listingID= intent.getLongExtra("id",-1)
+
+
+        var imageList = ArrayList<Int>()
+        imageList.add(R.drawable.book)
+        imageList.add(R.drawable.duck)
+        imageList.add(R.drawable.car)
+        imageList.add(R.drawable.guitar)
+
+        var viewPagerAdapter = DetailViewPagerAdapter(this, imageList)
+
+        viewPager.adapter=viewPagerAdapter
 
         readThread(listingID)
 
-        //Todo: Create viewmodel for image, eventually get the image from Firebase, and change it from one picture to a "slideshow"
-        val imageView=findViewById<ImageView>(R.id.details_image)
-        imageView.setImageResource(R.drawable.duck)
+
+        val ONE_MEGABYTE: Long = 1024 * 1024
+        reference.child("listings/1/car.jpg").getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            Glide.with(this)  //this is the current Activity
+                .load(it)
+                //.into(imageView);
+        }.addOnFailureListener {
+            // Handle any errors
+            throw it
+        }
+
+
+
+
+        //imageView.setImageResource(R.drawable.duck)
     }
 
     //Read info from Firebase
---
+++ app/src/main/java/com/cmpt362/rentit/details/DetailViewPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.cmpt362.rentit.details
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.viewpager.widget.PagerAdapter
+import com.cmpt362.rentit.R
+import java.util.*
+
+class DetailViewPagerAdapter(val context: Context, val imageList: List<Int>): PagerAdapter(){
+
+
+
+    override fun getCount(): Int {
+        return imageList.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val mLayoutInflater =  context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView: View = mLayoutInflater.inflate(R.layout.details_image, container, false)
+        val imageView: ImageView = itemView.findViewById<View>(R.id.details_imageView) as ImageView
+
+        imageView.setImageResource(imageList.get(position))
+        Objects.requireNonNull(container).addView(itemView)
+        return itemView
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        // on below line we are removing view
+        container.removeView(`object` as LinearLayout)
+    }
+}
