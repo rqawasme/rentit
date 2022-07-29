1da28e1cc3945c20d83434b820fd762cb9a505f9
knakano <kei_nakano@sfu.ca>
2022-07-29 14:33:06 -0700

    Contact dialog

--- app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
+++ app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
@@ -7,23 +7,29 @@ import android.view.View
 import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.cmpt362.rentit.R
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
 class DetailActivity:AppCompatActivity() {
     private lateinit var db: FirebaseDatabase
     private var name:String?=""
-    var listingID:Long =-1
+    private var listingID:Long =-1
+    private var posterID:Int? =-1
     private var price:Float? =0f
     private var description:String? =""
-    lateinit var viewPager: ViewPager
-    var viewPagerAdapter: PagerAdapter? =null
+    private lateinit var viewPager: ViewPager
+    private var viewPagerAdapter: PagerAdapter? =null
+
+    private var phoneNumber:String? =""
+    private var email:String? =""
 
     val ONE_MEGABYTE: Long = 1024 * 1024
 
@@ -33,6 +39,7 @@ class DetailActivity:AppCompatActivity() {
 
         listingID= intent.getLongExtra("id",-1)
         var imageList = ArrayList<ByteArray>()
+        db= Firebase.database
 
         readThread(listingID)
         loadImages(imageList)
@@ -43,7 +50,6 @@ class DetailActivity:AppCompatActivity() {
         var reference = Firebase.storage.reference
         viewPager = findViewById(R.id.detail_viewpager)
         Thread(){
-            println(listingID)
             reference.child("listings/" + listingID + "/").listAll().addOnSuccessListener {
                 for(entry in it.items){
                     entry.getBytes(ONE_MEGABYTE*50).addOnSuccessListener {
@@ -59,7 +65,6 @@ class DetailActivity:AppCompatActivity() {
     //Read info from Firebase
     private fun readThread(listingID:Long){
         Thread(){
-            db= Firebase.database
             val myRefListings=db.getReference("Listings").child(listingID.toString())
 
             myRefListings.get().addOnCompleteListener{ task->
@@ -68,6 +73,7 @@ class DetailActivity:AppCompatActivity() {
                     name=snapshot.child("name").getValue(String::class.java)
                     price=snapshot.child("price").getValue(Float::class.java)
                     description=snapshot.child("description").getValue(String::class.java)
+                    posterID=snapshot.child("postUserID").getValue(Int::class.java)
 
                     val nameTextView=findViewById<TextView>(R.id.details_name)
                     val priceTextView=findViewById<TextView>(R.id.details_price)
@@ -78,6 +84,9 @@ class DetailActivity:AppCompatActivity() {
                     nameTextView.text = name
                     priceTextView.text= "$" + String.format("%.2f",price)
                     descriptionTextView.text=description
+
+                    //Get contact info from Firebase, after we get the posterID.
+                    getContactInfo(posterID!!)
                 }
                 else{
                     Log.d("TAG", task.exception!!.message!!)
@@ -86,11 +95,36 @@ class DetailActivity:AppCompatActivity() {
         }.start()
     }
 
+    private fun getContactInfo(userID:Int){
+        Thread(){
+            val myRefListings=db.getReference("Users").orderByChild("id").equalTo(userID.toDouble()).limitToFirst(1)
+
+            //Maybe a more smarter way to do this.
+            myRefListings.get().addOnCompleteListener{ task->
+                val snapshot= task.result.children
+                for (i in snapshot){
+                    phoneNumber= i.child("phone").getValue(String::class.java)
+                    email= i.child("email").getValue(String::class.java)
+                }
+            }
+        }.start()
+
+    }
+
     fun book(view: View){
         println("DEBUG: Book dialog open")
     }
 
+    //Open a dialog with contact info of user posting.
     fun contact(view: View){
+        if(phoneNumber==""){
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, "Fetching information from Firebase, please wait!", duration)
+            toast.show()
+        }
+        else{
+            DialogContact(email!!,phoneNumber!!).show(supportFragmentManager,DialogContact.TAG)
+        }
         println("DEBUG: Contact dialog open")
     }
 
