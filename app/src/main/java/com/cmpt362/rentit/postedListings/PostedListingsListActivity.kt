426ad9d66d7c5b3b2fe3a2df97e255036f458865
Kenneth Liang <kenneth-liang@hotmail.com>
2022-07-30 12:54:17 -0700

    circular profile image border

---
+++ app/src/main/java/com/cmpt362/rentit/postedListings/PostedListingsListActivity.kt
@@ -0,0 +1,12 @@
+package com.cmpt362.rentit.postedListings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cmpt362.rentit.R
+
+class PostedListingsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posted_listings_list)
+    }
+}
