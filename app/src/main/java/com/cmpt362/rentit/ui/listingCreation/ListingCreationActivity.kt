2b07f2d4cf87be461cce317bc95a88ad58951045
Danyaal Patel <danyaalp@sfu.ca>
2022-07-30 11:50:26 -0700

    added skeleton for ListingCreation Activity

---
+++ app/src/main/java/com/cmpt362/rentit/ui/listingCreation/ListingCreationActivity.kt
@@ -0,0 +1,11 @@
+package com.cmpt362.rentit.ui.listingCreation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListingCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listing_creation)
+    }
+}
