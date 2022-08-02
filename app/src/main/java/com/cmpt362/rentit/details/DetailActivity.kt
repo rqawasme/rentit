5da61ab5ec7e0795edb4f7243e0bd6d57a7bed9b
Rashonmyeed <qawasmehr55@gmail.com>
2022-08-01 18:20:40 -0700

    Markers show and load images

--- app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
+++ app/src/main/java/com/cmpt362/rentit/details/DetailActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.cmpt362.rentit.Constants.ONE_MEGABYTE
 import com.cmpt362.rentit.R
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
@@ -30,8 +31,6 @@ class DetailActivity:AppCompatActivity() {
 
     private var phoneNumber:String? =""
     private var email:String? =""
-
-    val ONE_MEGABYTE: Long = 1024 * 1024
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/cmpt362/rentit/ui/home/MapDialog.kt
+++ app/src/main/java/com/cmpt362/rentit/ui/home/MapDialog.kt
@@ -4,17 +4,13 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
-import android.util.Log
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.cmpt362.rentit.R
+import com.cmpt362.rentit.Utils.getDescriptionSnippet
+import com.cmpt362.rentit.Utils.getImage
 import com.cmpt362.rentit.details.DetailActivity
-import com.cmpt362.rentit.details.DetailViewPagerAdapter
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import kotlin.properties.Delegates
 
 class MapDialog: DialogFragment() {
@@ -40,11 +36,12 @@ class MapDialog: DialogFragment() {
         }
         val view = requireActivity().layoutInflater.inflate(R.layout.dialog_map, null)
         imageView = view.findViewById(R.id.mapDialogImage)
+        getImage(key, imageView)
         textView = view.findViewById(R.id.mapDialogText)
+        getDescriptionSnippet(key, textView)
         val builder = AlertDialog.Builder(requireActivity())
         builder.setView(view)
         builder.setTitle(name)
-        textView.text = name
         builder.setPositiveButton(getString(R.string.view_more)) { dialog, id ->
             println("DEBUG: We will open the detail view now for $name")
             val intent = Intent(requireActivity(), DetailActivity::class.java)
@@ -57,55 +54,4 @@ class MapDialog: DialogFragment() {
         dialog = builder.create()
         return dialog
     }
-
-//
-//    //Load images from Firebase to ViewPager
-//    private fun loadImages(imageList: ArrayList<ByteArray>){
-//        var reference = Firebase.storage.reference
-//        viewPager = findViewById(R.id.detail_viewpager)
-//        Thread(){
-//            reference.child("listings/" + listingID + "/").listAll().addOnSuccessListener {
-//                for(entry in it.items){
-//                    entry.getBytes(ONE_MEGABYTE*50).addOnSuccessListener {
-//                        imageList.add(it)
-//                        viewPagerAdapter = DetailViewPagerAdapter(this, imageList)
-//                        viewPager.adapter=viewPagerAdapter
-//                    }
-//                }
-//            }
-//        }.start()
-//    }
-//
-//    //Read info from Firebase
-//    private fun readThread(listingID:Long){
-//        Thread(){
-//            val myRefListings=db.getReference("Listings").child(listingID.toString())
-//
-//            myRefListings.get().addOnCompleteListener{ task->
-//                if(task.isSuccessful){
-//                    val snapshot= task.result
-//                    name=snapshot.child("name").getValue(String::class.java)
-//                    price=snapshot.child("price").getValue(Float::class.java)
-//                    description=snapshot.child("description").getValue(String::class.java)
-//                    posterID=snapshot.child("postUserID").getValue(String::class.java)
-//
-//                    val nameTextView=findViewById<TextView>(R.id.details_name)
-//                    val priceTextView=findViewById<TextView>(R.id.details_price)
-//                    val descriptionTextView=findViewById<TextView>(R.id.details_description)
-//
-//                    descriptionTextView.movementMethod= ScrollingMovementMethod()
-//
-//                    nameTextView.text = name
-//                    priceTextView.text= "$" + String.format("%.2f",price)
-//                    descriptionTextView.text=description
-//
-//                    //Get contact info from Firebase, after we get the posterID.
-//                    getContactInfo(posterID!!)
-//                }
-//                else{
-//                    Log.d("TAG", task.exception!!.message!!)
-//                }
-//            }
-//        }.start()
-//    }
 }
