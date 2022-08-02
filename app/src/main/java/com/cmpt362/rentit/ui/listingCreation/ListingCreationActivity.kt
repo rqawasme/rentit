02ff5c555255bd824b232ba8a45f083d8c333961
Danyaal Patel <danyaalp@sfu.ca>
2022-08-02 15:35:47 -0700

    simple interface is there, however backend not implemented yet for listing creation

--- app/src/main/java/com/cmpt362/rentit/ui/listingCreation/ListingCreationActivity.kt
+++ app/src/main/java/com/cmpt362/rentit/ui/listingCreation/ListingCreationActivity.kt
@@ -2,6 +2,7 @@ package com.cmpt362.rentit.ui.listingCreation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cmpt362.rentit.R
 
 class ListingCreationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
