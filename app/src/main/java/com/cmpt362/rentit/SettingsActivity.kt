d88d061c0db695ceaee8d8801256df0cfe6b7814
Kenneth Liang <kenneth-liang@hotmail.com>
2022-08-03 01:23:22 -0700

    update email activity complete

--- app/src/main/java/com/cmpt362/rentit/SettingsActivity.kt
+++ app/src/main/java/com/cmpt362/rentit/SettingsActivity.kt
@@ -1,10 +1,15 @@
 package com.cmpt362.rentit
 
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var preference: Preference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +20,13 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
         }
     }
 }
