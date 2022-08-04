8bd1d804bf774c320e0baa841b351481485268ec
Kenneth Liang <kenneth-liang@hotmail.com>
2022-08-04 12:30:51 -0700

    user have to have a least 1 photo per listing

--- app/src/main/java/com/cmpt362/rentit/userListings/UserListingsListAdapter.kt
+++ app/src/main/java/com/cmpt362/rentit/userListings/UserListingsListAdapter.kt
@@ -2,15 +2,19 @@ package com.cmpt362.rentit.userListings
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import com.cmpt362.rentit.Constants
 import com.cmpt362.rentit.R
+import com.cmpt362.rentit.Utils
 import com.cmpt362.rentit.db.Listing
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class UserListingsListAdapter(private val context: Activity, private val listingArrayList: ArrayList<Listing>): BaseAdapter() {
     private var layoutInflater: LayoutInflater? = null
@@ -32,22 +36,23 @@ class UserListingsListAdapter(private val context: Activity, private val listing
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            var convertView = convertView
-            if (layoutInflater == null) {
-                layoutInflater =
-                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            }
-            if (convertView == null) {
-                convertView = layoutInflater!!.inflate(R.layout.user_listing_item, null)
-            }
-            imageViewListingImage = convertView!!.findViewById(R.id.userListing_imageView_listingImage)
-            textViewListingTitle = convertView.findViewById(R.id.userListings_textView_listingTitle)
-            textViewListingPrice = convertView.findViewById(R.id.userListings_textView_listingPrice)
+        var convertView = convertView
+        if (layoutInflater == null) {
+            layoutInflater =
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+        if (convertView == null) {
+            convertView = layoutInflater!!.inflate(R.layout.user_listing_item, null)
+        }
+        imageViewListingImage = convertView!!.findViewById(R.id.userListing_imageView_listingImage)
+        textViewListingTitle = convertView.findViewById(R.id.userListings_textView_listingTitle)
+        textViewListingPrice = convertView.findViewById(R.id.userListings_textView_listingPrice)
 
 
-            textViewListingTitle.text = listingArrayList[position].name
-            textViewListingPrice.text = listingArrayList[position].price.toString()
+        textViewListingTitle.text = listingArrayList[position].name
+        textViewListingPrice.text = listingArrayList[position].price.toString()
 
+        Utils.getImage(listingArrayList[position].listingID!!, imageViewListingImage)
 
         return convertView
     }
