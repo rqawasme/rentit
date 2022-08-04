ba0841c9006a898fb8c0c5c3db0785343d1a4a69
knakano <kei_nakano@sfu.ca>
2022-08-03 19:33:05 -0700

    Your Bookings Functional

--- app/src/main/java/com/cmpt362/rentit/db/Booking.kt
+++ app/src/main/java/com/cmpt362/rentit/db/Booking.kt
@@ -7,7 +7,7 @@ data class Booking(
     val id:String?= null,
     val startTime: String? = null, //Note: Saved in format HH:mm MMMM dd yyyy
     val endTime: String? = null,
-    val listingID: String? = null,
+    val listingID: String?= null,
     val bookerID:String? = null ) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
