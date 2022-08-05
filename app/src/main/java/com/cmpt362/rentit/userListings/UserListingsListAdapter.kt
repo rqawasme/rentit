b29d3cffdf485202d81e533a7876c4d4f4a7aa73
Kenneth Liang <kenneth-liang@hotmail.com>
2022-08-04 19:18:10 -0700

    added buttonavailability on click, not work yet

--- app/src/main/java/com/cmpt362/rentit/userListings/UserListingsListAdapter.kt
+++ app/src/main/java/com/cmpt362/rentit/userListings/UserListingsListAdapter.kt
@@ -21,6 +21,9 @@ class UserListingsListAdapter(private val context: Activity, private val listing
     private lateinit var imageViewListingImage: ImageView
     private lateinit var textViewListingTitle: TextView
     private lateinit var textViewListingPrice: TextView
+    private lateinit var textViewAvailability: TextView
+    private lateinit var buttonEditListing: Button
+    private lateinit var buttonAvailability: Button
 
     override fun getCount(): Int {
         return listingArrayList.size
@@ -47,14 +50,46 @@ class UserListingsListAdapter(private val context: Activity, private val listing
         imageViewListingImage = convertView!!.findViewById(R.id.userListing_imageView_listingImage)
         textViewListingTitle = convertView.findViewById(R.id.userListings_textView_listingTitle)
         textViewListingPrice = convertView.findViewById(R.id.userListings_textView_listingPrice)
-
+        textViewAvailability = convertView.findViewById(R.id.userListings_textView_listingAvailability)
+        buttonEditListing = convertView.findViewById(R.id.userListings_button_editListing)
+        buttonAvailability = convertView.findViewById(R.id.userListings_button_availability)
 
         textViewListingTitle.text = listingArrayList[position].name
         textViewListingPrice.text = listingArrayList[position].price.toString()
 
+        if (!listingArrayList[position].available){
+           markUnavailable(convertView)
+        }
+        else{
+            markAvailable(convertView)
+        }
+
+        buttonAvailability.setOnClickListener {
+            if (textViewAvailability.text == Constants.AVAILABLE_TEXT){
+                markUnavailable(convertView)
+            }
+            else if (textViewAvailability.text == Constants.UNAVAILABLE_TEXT){
+                markUnavailable(convertView)
+            }
+        }
+
         Utils.getImage(listingArrayList[position].listingID!!, imageViewListingImage)
 
         return convertView
+    }
+
+    fun markAvailable(view: View){
+        textViewAvailability.text = Constants.AVAILABLE_TEXT
+        buttonAvailability.text = Constants.MARK_UNAVAILABLE_TEXT
+        val drawableLeft = view.resources.getDrawable(R.drawable.ic_baseline_not_interested_24)
+        buttonAvailability.setCompoundDrawables(drawableLeft, null, null, null)
+    }
+
+    fun markUnavailable(view: View){
+        textViewAvailability.text = Constants.UNAVAILABLE_TEXT
+        buttonAvailability.text = Constants.MARK_AVAILABLE_TEXT
+        val drawableLeft = view.resources.getDrawable(R.drawable.ic_baseline_event_available_24)
+        buttonAvailability.setCompoundDrawables(drawableLeft, null, null, null)
     }
 
 }
