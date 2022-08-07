19331113a2184cf211fdb4695a7743d7ef292171
Kenneth Liang <kenneth-liang@hotmail.com>
2022-08-07 03:06:35 -0700

    improved detail listing activity

--- app/src/main/java/com/cmpt362/rentit/ui/bookingList/BookingListAdapter.kt
+++ app/src/main/java/com/cmpt362/rentit/ui/bookingList/BookingListAdapter.kt
@@ -56,55 +56,48 @@ class BookingListAdapter(private val context: Activity, private val bookingArray
         var textViewBookingEndTime = convertView!!.findViewById<TextView>(R.id.user_booking_time_to)
         textViewBookingEndTime.setText(bookingArrayList[position].first.endTime)
 
-
-
         titleView= convertView!!.findViewById<TextView>(R.id.user_booking_title)
         titleView.setText(bookingArrayList[position].second)
 
         //Add onclickListener to buttons
         var seeListingButton= convertView!!.findViewById<Button>(R.id.user_booking_see_listing)
-        var isListingExist = false
-
-        val myRefListings= Firebase.database.getReference(Constants.LISTINGS_PATH)
-        val listingID = bookingArrayList[position].first.listingID
-        val listingDataSnapshot = myRefListings.child(listingID!!).get()
-        listingDataSnapshot.addOnSuccessListener {
-            if (it.exists()){
-                isListingExist = true
-                seeListingButton.setOnClickListener{
-                    val intent = Intent(convertView.context, DetailActivity::class.java)
-                    intent.putExtra("id",bookingArrayList[position].first.listingID)
-                    context.startActivity(intent)
-                }
-            }
-            else{
-                isListingExist = false
-                seeListingButton.isEnabled = false
-            }
-        }
 
         var activeIndicator=convertView!!.findViewById<TextView>(R.id.booking_active)
 
-        //Check if active
         var bookingStartMilli= SimpleDateFormat("HH:mm MMMM dd yyyy").parse(bookingArrayList[position].first.startTime ).time
         var bookingEndMilli= SimpleDateFormat("HH:mm MMMM dd yyyy").parse(bookingArrayList[position].first.endTime).time
 
         var currentMilli= Date().time
 
-        if((currentMilli<=bookingEndMilli) && (currentMilli>=bookingStartMilli)){
-            activeIndicator.setText("Active")
-            activeIndicator.setBackgroundResource(R.drawable.green_rounded_background)
-        }
-        else{
-            if (isListingExist){
-                activeIndicator.setText("Inactive")
-                activeIndicator.setBackgroundResource(R.drawable.red_rounded_background)
+        val myRefListings= Firebase.database.getReference(Constants.LISTINGS_TABLE_NAME)
+        val listingID = bookingArrayList[position].first.listingID
+        val listingDataSnapshot = myRefListings.child(listingID!!).get()
+        listingDataSnapshot.addOnSuccessListener {
+            if (it.exists()){
+                seeListingButton.setOnClickListener{
+                    val intent = Intent(convertView.context, DetailActivity::class.java)
+                    intent.putExtra("id",bookingArrayList[position].first.listingID)
+                    context.startActivity(intent)
+                }
+
+                if((currentMilli <= bookingEndMilli) && (currentMilli >= bookingStartMilli)){
+                    activeIndicator.setText("In progress")
+                    activeIndicator.setBackgroundResource(R.drawable.rentit_accent_rounded_background)
+                }
+                else if (currentMilli > bookingEndMilli){
+                    activeIndicator.setText("Finished")
+                    activeIndicator.setBackgroundResource(R.drawable.green_rounded_background)
+                }
+                else if (currentMilli < bookingStartMilli){
+                    activeIndicator.setText("Awaiting")
+                    activeIndicator.setBackgroundResource(R.drawable.blue_rounded_background)
+                }
             }
             else{
+                seeListingButton.isEnabled = false
                 activeIndicator.setText("Listing deleted/unavailable")
                 activeIndicator.setBackgroundResource(R.drawable.dark_grey_rounded_background)
             }
-
         }
 
         return convertView
